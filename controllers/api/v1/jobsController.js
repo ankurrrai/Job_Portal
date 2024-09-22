b496fa2e55a9b2f772cf46c5bc9d5f18eeb3814c
ankurrrai <ankur.rai5666@gmail.com>
2024-09-22 20:50:02 +0530

    Updated till update job and delete job

--- controllers/api/v1/jobsController.js
+++ controllers/api/v1/jobsController.js
@@ -37,3 +37,49 @@ export const getAllJob=async (req,res,next) => {
         next(error)
     }
 }
+
+export const deleteJob=async (req,res,next)=>{
+    // await Jobs.deleteMany({createdBy:req.params.id})
+    // return res.status(200).json({
+    //     message:"Success"
+    // })
+    try {
+        let jobId=req.params.id;
+        let job=await Jobs.findById(jobId);
+        if (job){
+            if (job.createdBy.toString()===req.user.userId){
+                await Jobs.deleteOne({_id:jobId});
+                return res.status(200).json({
+                    messsage:'Job deleted successfully!'
+                })
+            } else {
+                return next("Invalid Job Details")
+            }
+        }
+        return next("Invalid Job Details")
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateJob=async (req,res,next)=>{
+    try {
+        let jobId=req.params.id;
+        let job=await Jobs.findById(jobId);
+
+        if (job){
+            if (job.createdBy.toString()===req.user.userId){
+                let updatedJob= await Jobs.findByIdAndUpdate(jobId,req.body,{new:true,runValidators:true})
+                return res.status(200).json({
+                    messsage:'Job deleted successfully!',
+                    Job:updatedJob
+                })
+            } else {
+                return next("Invalid Job Details")
+            }
+        }
+        return next("Invalid Job Details")
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/api/v1/job.js
+++ routes/api/v1/job.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { authReq } from '../../../middlewares/authMiddkeware.js'
-import { createJob, getAllJob } from "../../../controllers/api/v1/jobsController.js"
+import { createJob, deleteJob, getAllJob, updateJob } from "../../../controllers/api/v1/jobsController.js"
 const router=express.Router();
 
 router.post('/create',authReq,createJob);
 router.get('/get-all-job',authReq,getAllJob);
+router.delete('/delete-job/:id',authReq,deleteJob)
+router.patch('/update-job/:id',authReq,updateJob)
 
 export default router
